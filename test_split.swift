63779fdda7f2776c90215fdc0658a6f38ec45ef7
yude <i@yude.jp>
2026-08-26 20:01:11 +0900

    chore: remove test and script files

--- test_split.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-let line = "drwxr-xr-x   2 user     group        4096 Aug 17 10:00 dir name"
-let parts = line.split(separator: " ", maxSplits: 8)
-for (i, p) in parts.enumerated() {
-    print("\(i): '\(p)'")
-}
